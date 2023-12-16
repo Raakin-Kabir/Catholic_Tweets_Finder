06a0c8bfc6ed51dba9ca9fe84124aa64af3dcdff
TheUnnoticable <sharpenernewt@gmail.com>
2023-12-16 18:36:25 -0500

    Modified

--- cath_tweet_finder.py
+++ cath_tweet_finder.py
@@ -1,9 +1,8 @@
 from transformers import pipeline
 
-classifier = pipeline("zero-shot-classification",
-                      model="facebook/bart-large-mnli")
-sequence_to_classify = "The only innocent feature in babies is the weakness of their frames; the minds of infants are far from innocent."
-candidate_labels = ['absurd', 'rational']
+classifier = pipeline("zero-shot-classification", model="MoritzLaurer/DeBERTa-v3-base-mnli-fever-anli")
+sequence_to_classify = "Gravity exists because all things desire unity, and the greater another thing is, the more that things desire unity with it."
+candidate_labels = ['Theistic', 'Atheistic']
 print(classifier(sequence_to_classify, candidate_labels))
 
 
